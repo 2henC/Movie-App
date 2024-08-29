e2312f302c344606a04aedc75229dcd9b7f44f52
2henC <nguyenthanh8a2@gmail.com>
2024-08-30 01:39:05 +0700

    set up project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
   return (
     <>
-      <p className="font-bold">This is a new my project</p>
+      <header className=" flex bg-slate-900 h-14 text-white justify-between items-center px-8">
+        <div className="flex items-center gap-4">
+          <img src="./img/netflix.png" className="w-12 sm:w-12"></img>
+          <div>Phim</div>
+          <div>Truyền hình</div>
+        </div>
+        <div>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="cursor-pointer"
+          />
+        </div>
+      </header>
     </>
   );
 }
