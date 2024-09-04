4b6087c4a38d0fe5e98cf2d54d38a6dda9fd38a3
2henC <nguyenthanh8a2@gmail.com>
2024-09-04 21:17:44 +0700

    load data from API to distribute tabs All, TV Show, Movie

--- src/components/MediaList/index.jsx
+++ src/components/MediaList/index.jsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
+const TABS = [
+  { id: "all", value: "All" },
+  { id: "tv", value: "TV Show" },
+  { id: "movie", value: "Movie" },
+];
+
 const MeadiaList = () => {
   const [meidaList, setMediaList] = useState([]);
+  const [mediaTabId, setMediaTabId] = useState("all");
 
   useEffect(() => {
-    fetch("https://api.themoviedb.org/3/trending/all/day", {
+    fetch(`https://api.themoviedb.org/3/trending/${mediaTabId}/day`, {
       method: "GET",
       headers: {
         accept: "application/json",
@@ -17,18 +24,27 @@ const MeadiaList = () => {
       setMediaList(popularMovies);
       console.log(popularMovies);
     });
-  }, []);
+  }, [mediaTabId]);
 
   return (
     <div className="bg-black px-8 text-[1.2vw] text-white">
       <div className="flex items-center gap-4 py-8">
         <p className="text-[2vw]">Trending</p>
         <ul className="flex rounded-md border-2 border-white">
-          <li className="cursor-pointer rounded-sm bg-white px-2 py-1 text-black">
+          {/* <li className="cursor-pointer rounded-sm bg-white px-2 py-1 text-black">
             All
           </li>
           <li className="cursor-pointer rounded-md px-2 py-1">Movie</li>
-          <li className="cursor-pointer rounded-md px-2 py-1">TV Show</li>
+          <li className="cursor-pointer rounded-md px-2 py-1">TV Show</li> */}
+          {TABS.map((tab) => (
+            <li
+              key={tab.id}
+              className={`cursor-pointer rounded-sm px-2 py-1 ${tab.id === mediaTabId ? "bg-white text-black" : ""}`}
+              onClick={() => setMediaTabId(tab.id)}
+            >
+              {tab.value}
+            </li>
+          ))}
         </ul>
       </div>
 
